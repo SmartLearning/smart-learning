60a72a055fc5bf2cd9dee876b7fd1c858915fe93
Navid Ghahremani <ghahramani.navid@gmail.com>
2017-07-03 14:40:51 +0300

    fixed cache template problem

--- app/gulp/config.js
+++ app/gulp/config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var exports = {
-    moduleName: 'gatewayApp',
+    moduleName: 'smartApp',
     app: 'src/main/webapp/',
     dist: 'build/www/',
     swaggerDist: 'build/www/swagger-ui/',
