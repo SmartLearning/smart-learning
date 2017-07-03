f24fe12c8af989f3c67987a6c650e21a9d4c3589
Navid Ghahremani <ghahramani.navid@gmail.com>
2017-07-03 09:45:16 +0300

    fixed some problem of building war file read mongodb url from environment to compatible with heroku

--- app/src/main/webapp/app/blocks/BlocksConstants.js
+++ app/src/main/webapp/app/blocks/BlocksConstants.js
@@ -4,6 +4,6 @@
     angular
         .module('app.blocks')
         .constant('VERSION', "1.0.0")
-        .constant('DEBUG_INFO_ENABLED', true)
+        .constant('DEBUG_INFO_ENABLED', false)
 ;
 })(angular);
