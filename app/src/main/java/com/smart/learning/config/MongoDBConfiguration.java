be51a0778ba58163264c96d27dbfbcd3645b7df0
Navid Ghahremani <ghahramani.navid@gmail.com>
2017-07-03 10:14:04 +0300

    changed mongobee config

--- app/src/main/java/com/smart/learning/config/MongoDBConfiguration.java
+++ app/src/main/java/com/smart/learning/config/MongoDBConfiguration.java
@@ -1,7 +1,6 @@
 package com.smart.learning.config;
 
 import com.github.mongobee.Mongobee;
-import com.mongodb.MongoClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
@@ -46,9 +45,9 @@ public class MongoDBConfiguration {
     }
 
     @Bean
-    public Mongobee mongobee(MongoClient mongoClient, MongoTemplate mongoTemplate, MongoProperties mongoProperties) {
+    public Mongobee mongobee(MongoTemplate mongoTemplate, MongoProperties mongoProperties) {
         logger.debug("Configuring Mongobee");
-        Mongobee mongobee = new Mongobee(mongoClient);
+        Mongobee mongobee = new Mongobee(mongoProperties.getUri());
         mongobee.setDbName(mongoProperties.getDatabase());
         mongobee.setMongoTemplate(mongoTemplate);
         // package to scan for migrations
