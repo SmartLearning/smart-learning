7c9e8c4463f5d2ffe2de2bbf6d9d31e9c8c3cfa1
Navid Ghahremani <ghahramani.navid@gmail.com>
2017-09-13 00:40:23 +0100

    Changed url and variable name

--- js/AppController.js
+++ js/AppController.js
@@ -9,15 +9,26 @@
         .module('app')
         .controller('AppController', AppController);
 
-    AppController.$inject = ['$mdDialog','$sce'];
+    AppController.$inject = [
+        '$mdDialog',
+        '$sce'
+    ];
 
     /* @ngInject */
-    function AppController($mdDialog,$sce) {
+    function AppController($mdDialog, $sce) {
         var vm = this;
 
         vm.pages = [
-            {name: 'My Messages', url: $sce.trustAsResourceUrl('https://docs.google.com/document/d/1Z4bk8xSx-8AudSwHAdUT2KgBQsPi7PmiOzBwzPKVSt8/pub?embedded=true')},
-            {name: 'Unread Messages', url: $sce.trustAsResourceUrl('http://www.varzesh3.com')}
+            {
+                type: 'exam',
+                title: 'My Messages',
+                url: $sce.trustAsResourceUrl('https://docs.google.com/document/d/1Z4bk8xSx-8AudSwHAdUT2KgBQsPi7PmiOzBwzPKVSt8/pub?embedded=true')
+            },
+            {
+                type: 'exam',
+                title: 'Possessives',
+                url: $sce.trustAsResourceUrl('https://drive.google.com/open?id=1KZCIs_nVrB7NWP1F0N53UIH7VSUAJDNrUX58BiIx6uQ')
+            }
         ];
 
         vm.showTableOfContent = showTableOfContent;
