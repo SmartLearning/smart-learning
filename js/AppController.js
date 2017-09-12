df68b9a8ee94e88ed7004a180114f9c49a9bbdf1
Navid Ghahremani <ghahramani.navid@gmail.com>
2017-09-13 00:31:12 +0100

    Added dynamic url for iframe

--- js/AppController.js
+++ js/AppController.js
@@ -9,15 +9,15 @@
         .module('app')
         .controller('AppController', AppController);
 
-    AppController.$inject = ['$mdDialog'];
+    AppController.$inject = ['$mdDialog','$sce'];
 
     /* @ngInject */
-    function AppController($mdDialog) {
+    function AppController($mdDialog,$sce) {
         var vm = this;
 
         vm.pages = [
-            {name: 'My Messages'},
-            {name: 'Unread Messages'}
+            {name: 'My Messages', url: $sce.trustAsResourceUrl('https://docs.google.com/document/d/1Z4bk8xSx-8AudSwHAdUT2KgBQsPi7PmiOzBwzPKVSt8/pub?embedded=true')},
+            {name: 'Unread Messages', url: $sce.trustAsResourceUrl('http://www.varzesh3.com')}
         ];
 
         vm.showTableOfContent = showTableOfContent;
@@ -41,7 +41,7 @@
         }
 
         function changePage(item) {
-            vm.title = item.name;
+            vm.current = item;
         }
     }
 
